9ddbe906c44b6fe2226f65ac937f16e06ba0236c
soheb sultan <soheb@marketlytics.com>
2014-03-08 13:33:23 +0500

    initial committ
    Signed-off-by: soheb sultan <soheb@marketlytics.com>

---
+++ javascript/compare.js
@@ -0,0 +1,246 @@
+var object1 = {
+  _id: "http://www.gsmarena.com/samsung_i9295_galaxy_s4_active-5446.php",
+  link: "http://www.gsmarena.com/samsung_i9295_galaxy_s4_active-5446.php",
+  imagePath: "http://cdn2.gsmarena.com/vv/bigpic/samsung-i9295-galaxy-s4-active-ofic.jpg",
+  "2G Network": "GSM 850 / 900 / 1800 / 1900 ",
+  "3G Network": "HSDPA 850 / 900 / 1900 / 2100 HSDPA 850 / 1900 / 2100 ",
+  "4G Network": "LTE 800 / 850 / 900 / 1800 / 2100 / 2600 LTE 700 / 850 / 1700 / 1900 / 2100 / 2600 - SGH-I537",
+  SIM: "Micro-SIM",
+  Announced: "2013, June",
+  Status: "Available. Released 2013, June",
+  Dimensions: "139.7 x 71.3 x 9.1 mm (5.5 x 2.81 x 0.36 in)",
+  Weight: "153 g (5.40 oz)",
+  "Addon Body": "- IP67 certified - dust and water resistant\r\n- Water resistant up to 1 meter and 30 minutes",
+  Type: "TFT capacitive touchscreen, 16M colors",
+  Size: "1080 x 1920 pixels, 5.0 inches (~441 ppi pixel density)",
+  Multitouch: "Yes",
+  Protection: "Corning Gorilla Glass 2",
+  "Addon Display": "- TouchWiz UI\r\n",
+  "Alert types": "Vibration; MP3, WAV ringtones",
+  Loudspeaker: "\r\nVoice 72dB / Noise 66dB / Ring 78dB",
+  "Card slot": "microSD, up to 64 GB",
+  Internal: "16 GB (11.2 GB user available), 2 GB RAM",
+  GPRS: "Class 33",
+  EDGE: "Class 33",
+  Speed: "HSDPA, 42 Mbps; HSUPA, 5.76 Mbps; LTE, Cat3, 50 Mbps UL, 100 Mbps DL",
+  WLAN: "Wi-Fi 802.11 a/b/g/n/ac, dual-band, Wi-Fi Direct, DLNA, Wi-Fi hotspot",
+  Bluetooth: "Yes, v4.0 with A2DP, EDR, LE",
+  NFC: "Yes",
+  "Infrared port": "Yes",
+  USB: "Yes, microUSB v2.0 (MHL 2), USB On-the-go, USB Host",
+  Primary: "8 MP, 3264 x 2448 pixels, autofocus, LED flash, check quality",
+  Features: "Simultaneous HD video and image recording, geo-tagging, touch focus, face and smile detection, image stabilization, HDR, Aqua mode",
+  Video: "Yes, 1080p@30fps, check quality",
+  Secondary: "Yes, 2 MP,1080p@30fps",
+  OS: "Android OS, v4.2.2 (Jelly Bean)",
+  Chipset: "Qualcomm APQ8064T Snapdragon 600",
+  CPU: "Quad-core 1.9 GHz Krait 300",
+  GPU: "Adreno 320",
+  Sensors: "Accelerometer, gyro, proximity, compass, barometer, gesture",
+  Messaging: "SMS(threaded view), MMS, Email, Push Mail, IM, RSS",
+  Browser: "HTML5",
+  Radio: "No",
+  GPS: "Yes, with A-GPS support and GLONASS",
+  Java: "Yes, via Java MIDP emulator",
+  Colors: "Urban Grey, Dive Blue, Orange Flare",
+  "Addon Features": "- S-Voice natural language commands and dictation\r\n- Smart stay, Smart pause, Smart scroll\r\n- Air gestures\r\n- Dropbox (50 GB cloud storage)\r\n- Active noise cancellation with dedicated mic\r\n- TV-out (via MHL 2 A/V link)\r\n- SNS integration\r\n- MP4/DivX/XviD/WMV/H.264/H.263 player\r\n- MP3/WAV/eAAC+/AC3/FLAC player\r\n- Organizer\r\n- Image/video editor\r\n- Document viewer (Word, Excel, PowerPoint, PDF)\r\n- Google Search, Maps, Gmail,\r\nYouTube, Calendar, Google Talk, Picasa\r\n- Voice memo/dial/commands\r\n- Predictive text input (Swype)",
+  "Addon Battery": "Li-Ion 2600 mAh battery",
+  "Stand-by": "Up to 312 h",
+  "Talk time": "Up to 17 h",
+  "SAR US": "\r\n0.55 W/kg (head)     0.75 W/kg (body)     ",
+  "SAR EU": "\r\n0.26 W/kg (head)     0.36 W/kg (body)     ",
+  "Price group": "",
+  Display: "\r\nContrast ratio: 1046:1 (nominal), 2.022 (sunlight)",
+  "Audio quality": "\r\nNoise -96.1dB / Crosstalk -96.0dB",
+  Camera: "\r\nPhoto / Video",
+  "Battery life": "\r\nEndurance rating 57h",
+  "Addon Tests": ""
+};
+//console.log(object1);
+
+var object2 = {
+  _id: "http://www.gsmarena.com/htc_one-5313.php",
+  title: "HTC One",
+  link: "http://www.gsmarena.com/htc_one-5313.php",
+  imagePath: "http://cdn2.gsmarena.com/vv/bigpic/htc-one-m7-new1.jpg",
+  "2G Network": "GSM 850 / 900 / 1800 / 1900 ",
+  "3G Network": "HSDPA 850 / 900 / 1900 / 2100 ",
+  "4G Network": "LTE 800 / 1800 / 2600  LTE 1800 / 2600LTE 1900 - for SprintLTE 700 / 850 / 1700 / 1900 / 2100 - for AT&TLTE 700 / 1700 / 2100 - for T-Mobile",
+  SIM: "Micro-SIM",
+  Announced: "2013, February",
+  Status: "Available. Released 2013, March",
+  Dimensions: "137.4 x 68.2 x 9.3 mm (5.41 x 2.69 x 0.37 in)",
+  Weight: "143 g (5.04 oz)",
+  Type: "Super LCD3 capacitive touchscreen, 16M colors",
+  Size: "1080 x 1920 pixels, 4.7 inches (~469 ppi pixel density)",
+  Multitouch: "Yes",
+  Protection: "Corning Gorilla Glass 2",
+  "Addon Display": "- HTC Sense UI v5",
+  "Alert types": "Vibration, MP3, WAV ringtones",
+  Loudspeaker: "\r\nVoice 69dB / Noise 66dB / Ring 75dB",
+  "Addon Sound": "- Beats Audio sound enhancement",
+  "Card slot": "No",
+  Internal: "32/64 GB, 2 GB RAM",
+  GPRS: "Yes",
+  EDGE: "Yes",
+  Speed: "HSPA+; LTE, Cat3, 50 Mbps UL, 100 Mbps DL",
+  WLAN: "Wi-Fi 802.11 a/b/g/n/ac, Wi-Fi Direct, DLNA, Wi-Fi hotspot",
+  Bluetooth: "Yes, v4.0 with A2DP",
+  NFC: "Yes (Market dependent)",
+  "Infrared port": "Yes",
+  USB: "Yes, microUSB v2.0 (MHL), USB On-the-go, USB Host",
+  Primary: "4 MP, 2688 x 1520 pixels, autofocus, optical image stabilization, LED flash, check quality",
+  Features: "1/3'' sensor size, 2�m pixel size, simultaneous HD video and image recording, geo-tagging, face and smile detection",
+  Video: "Yes, 1080p@30fps, 720p@60fps, HDR, stereo sound rec., video stabilization, check quality",
+  Secondary: "Yes, 2.1 MP, 1080p@30fps, HDR",
+  OS: "Android OS, v4.1.2 (Jelly Bean), v4.3 (Jelly Bean), planned upgrade to v4.4 (KitKat)",
+  Chipset: "Qualcomm APQ8064T Snapdragon 600",
+  CPU: "Quad-core 1.7 GHz Krait 300",
+  GPU: "Adreno 320",
+  Sensors: "Accelerometer, gyro, proximity, compass",
+  Messaging: "SMS (threaded view), MMS, Email, Push Email",
+  Browser: "HTML5",
+  Radio: "Stereo FM radio with RDS",
+  GPS: "Yes, with A-GPS support and GLONASS",
+  Java: "Yes, via Java MIDP emulator",
+  Colors: "Black, Silver, Red, Blue, Gold",
+  "Addon Features": "- SNS integration\r\n- Dropbox (25 GB cloud storage)\r\n- Active noise cancellation with dedicated mic\r\n- TV-out (via MHL A/V link)\r\n- DivX/XviD/MP4/H.263/H.264/WMV player\r\n- MP3/eAAC+/WMA/WAV/FLAC player\r\n- Google Search, Maps, Gmail,\r\nYouTube, Calendar, Google Talk\r\n- Organizer\r\n- Document viewer/editor\r\n- Photo viewer/editor\r\n- Voice memo/dial/commands\r\n- Predictive text input",
+  "Addon Battery": "Non-removable Li-Po 2300 mAh battery",
+  "Stand-by": "Up to 500 h (2G) / Up to 480 h (3G)",
+  "Talk time": "Up to 27 h (2G) / Up to 18 h (3G)",
+  "SAR US": "\r\n0.66 W/kg (head)     0.40 W/kg (body)     ",
+  "SAR EU": "\r\n0.86 W/kg (head)     0.36 W/kg (body)     ",
+  "Price group": "",
+  Display: "\r\nContrast ratio: 1541:1 (nominal) / 2.504:1 (sunlight)",
+  "Audio quality": "\r\nNoise -92.4dB / Crosstalk -92.4dB",
+  Camera: "\r\nPhoto / Video",
+  "Battery life": "\r\nEndurance rating 48h",
+  "Addon Tests": ""
+};
+//console.log(object2);
+
+var object1Internal = object1.Internal; //Retrieving phone1 internal memory field string
+var object2Internal = object2.Internal; //Retrieving phone2 internal memory field string
+var object1Size = 0;
+var object2Size = 0;
+
+// Calculating memory size for phone1
+var memory_size_object1 = function() {
+
+  // Iterating into memory field string
+  for (var i = 0; i < object1Internal.length; i++) {
+
+    //If size in GB, do this math
+    if(object1Internal[i] == "G") {
+      object1Size = parseInt(object1Internal);
+      object1Size *= 1024;
+      console.log("Phone1 can store " +parseInt(object1Size/6) + " songs.");
+      console.log("Phone1 can store " +parseInt(object1Size/11) + " minutes of HD video.");
+      break;
+    }
+
+    // Else, data will be MB. No math involved
+    else if(object1Internal[i] == "M") {
+      object1Size = parseInt(object1Internal);
+      console.log("Phone1 can store " +parseInt(object1Size) + " songs.");
+      console.log("Phone1 can store " +parseInt(object1Size/11) + " minutes of HD video.");
+      break;
+    }
+  };
+};
+memory_size_object1();
+
+// Calculating memory size for phone2
+var memory_size_object2 = function() {
+
+  // Iterating into memory field string
+  for (var i = 0; i < object2Internal.length; i++) {
+
+    //If size in GB, do this math
+    if(object2Internal[i] == "G") {
+      object2Size = parseInt(object2Internal);
+      object2Size *= 1024;
+      console.log("Phone2 can store " +parseInt(object2Size/6) + " songs.");
+      console.log("Phone2 can store " +parseInt(object2Size/11) + " minutes of HD video.");
+      break;
+    }
+
+    // Else, data will be MB. No math involved
+    else if(object2Internal[i] == "M") {
+      object2Size = parseInt(object2Internal);
+      console.log("Phone2 can store " +parseInt(object2Size) + " songs.");
+      console.log("Phone2 can store " +parseInt(object2Size/11) + " minutes of HD video.");
+      break;
+    }
+  };
+};
+memory_size_object2();
+
+/*var memory_comparsion = function() {
+  if (object1Size > object2Size) {
+    console.log ("Phone1 has more internal memory");
+  }
+  else console.log ("Phone2 has more memory");
+};
+memory_comparsion(); */
+
+var object1java=object1.Java; //Retrieving is object1 is java supported
+var object2java=object2.Java; //Retrieving is object2 is java supported
+
+var java_object1=function(){
+  for(var i = 0;i<object1java.length;i++){
+
+      if(object1java[i]=="Y"){
+        console.log("phone1 supports Java");
+      }
+      else if(object1java[i]=="N") {
+        console.log("phone1 doesnt supports Java");
+      }
+
+  }
+}
+java_object1();
+
+var java_object2=function(){
+  for(var i = 0;i<object2java.length;i++){
+
+      if(object2java[i]=="Y"){
+        console.log("phone2 supports Java");
+      }
+      else if(object2java[i]=="N"){
+        console.log("phone2 doesnt supports Java");
+      }
+
+  }
+}
+java_object2();
+
+
+
+var object1Weight = object1.Weight; 
+var object2Weight = object2.Weight;
+var object1Grams = 0;
+var object2Grams = 0;
+
+
+var weight_object1 = function() {
+  for (var i = 0; i < object1Weight.length; i++) {
+    if (object1Weight[i] == "g") {
+      object1Grams = parseInt(object1Weight);
+      console.log("The Weight of This Phone is " +object1Grams+"grams.");
+      break;
+    }
+  };
+};
+weight_object1();
+
+var weight_object2 = function() {
+  for (var i = 0; i < object2Weight.length; i++) {
+    if (object2Weight[i] == "g") {
+      object2Grams = parseInt(object2Weight);
+      console.log("The Weight of This Phone is " +object2Grams+ "grams.");
+      break;
+    }
+  };
+};
+weight_object2();
